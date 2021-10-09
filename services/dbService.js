a7baa114751d283f0183203e92153db4520d1bd0
Marc Gallagher-Reid <marc.gr@velocitypartners.com>
2021-10-09 08:49:24 +0100

    changed file structure for easier management

--- services/dbService.js
+++ services/dbService.js
@@ -1,4 +1,5 @@
 const MongoClient = require('mongodb').MongoClient;
+const calculateTweetScore = require('../functions/helpers/calculateTweetScore');
 const url = process.env.MONGO_URL;
 const dbname = 'stocks';
 const Client = new MongoClient(url, {useNewUrlParser: true, useUnifiedTopology: true});
@@ -20,4 +21,47 @@ function connectToDB(cb) {
     })
 }
 
+/**
+ * Adds a stock price and time to the db collection 'stocks'
+ *
+ * @param db database connection
+ *
+ * @param newUser object container new user data
+ *
+ * @param callback function
+ *
+ */
+
+ const addStockPrice = (db, companyName, stockPrice, callback) => {
+    let currentStockPrice = {
+        time  : Date.now() * 1000,
+        price : stockPrice
+    };
+
+    db.collection(companyName + 'StockPrice').insertOne(currentStockPrice, (err, result) => {
+        if (err) {
+            throw err;
+        }
+        callback(result);
+    });
+};
+
+const addTweetToDb = ( db, companyName, tweet, watsonAnalysis, callback ) => {
+    const score = calculateTweetScore.calculateTweetScore(tweet, watsonAnalysis);
+    let tweetData = {
+        time        : Date.now() * 1000,
+        tweetContent: tweet.data.text,
+        tweetData   : tweet
+    };
+
+    db.collection(companyName + 'Tweets').insertOne(tweetData, (err, result) => {
+        if (err) {
+            throw err;
+        }
+        callback(result);
+    });
+}
+
 module.exports.connectToDB = connectToDB;
+module.exports.addStockPrice = addStockPrice;
+module.exports.addTweetToDb  = addTweetToDb;
